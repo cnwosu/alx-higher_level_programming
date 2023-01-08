2418cb0997af296406a221b58d6a10fc4337050e
cnwosu <vera4amaka@yahoo.com>
2023-01-08 13:46:18 -0800

    bug fixes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
-import sys
-
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    argc = len(argv)
-    words = "argument"
-    period = '.'
+    import sys
 
-    if argc != 1:
-        words += 's'
-    if argc != 0:
-        period = ':'
-    print("{} {}{}".format(argc, words, period))
-    for i, arg in enumerate(argv, 1):
-        print("{}: {}".format(i, arg))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
