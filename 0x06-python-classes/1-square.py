a940298b6c3e30d5ac8c82d4381035d2f413c0a8
cnwosu <vera4amaka@yahoo.com>
2023-01-27 08:12:36 -0800

    bug fixes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,8 +6,8 @@ class Square:
     """Represents a square"""
 
     def __init__(self, size):
-    """Initializing this square class
-    Args: size - represnets the size of the square defined
-    """
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
 
-    self.__size = size
+        self.__size = size
